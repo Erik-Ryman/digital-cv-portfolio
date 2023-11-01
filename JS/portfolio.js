6d22fa7f92c2bb095beb521de03067bc76c76067
Erik Ryman <rymanerik@live.se>
2023-11-01 12:30:24 +0100

    updated lucky duck

---
+++ JS/portfolio.js
@@ -0,0 +1,45 @@
+const portfolio = document.querySelector(".portfolio");
+
+const displayPortfolio = () => {
+  projects.forEach((obj) => {
+    const html = `<div class="portfolio-container">
+    <p class="portfolio-description">${obj.description} <a
+    class="portfolio-link"
+    href=${obj.link}
+    >${obj.name}</a
+    >.
+    </p>
+    <img
+    class="portfolio-img"
+    src=${obj.image}
+    alt="${obj.name}"
+    />
+    </div>`;
+    portfolio.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+const projects = [
+  {
+    description:
+      "Här är ett grupprojekt där uppgiften var att tillverka en hemsida åt restaurangen",
+    name: "Lucky Duck",
+    link: "https://erik-ryman.github.io/Restaurang-Lucky-Duck/",
+    image: "./images/Misc/luckyDuck.png",
+  },
+  {
+    description:
+      "Här är ett projekt där uppgiften var att tillverka en hemsida åt",
+    name: "Fryele Ridskola",
+    link: "https://erik-ryman.github.io/Fryele-stallklubb-Erik/index.html",
+    image: "./images/Misc/fryele.png",
+  },
+  {
+    description: "Kommande projekt publiceras här när de är klara.",
+    name: "",
+    link: "",
+    image: "./images/Misc/questionmark.jpg",
+  },
+];
+
+displayPortfolio();
